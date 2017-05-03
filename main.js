6f3486dec3dd3e40d30c850248e5642fcc4e7824
F1ame <keeperflame45@gmail.com>
2017-05-03 15:52:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-
+function pageOne() {writedoccc = '<!doctype html>
+<head>
+                                   <script src="\main.js"></script>
+  <style>
+	  *:not(.cdc) {margin: 0px; padding=0px;background:black;border: none;}
+    @keyframes flash {
+      0% {color:lightgreen;} 
+      50% {color:lightgreen;}
+      51% {color:black;}
+      75% {color:black;}
+      76% {color:lightgreen;}
+      100% {color:lightgreen;}
+    }
+	  .flsh {animation: flash 1s 0s infinite;}  
+ .cdc { width: 40%;position: absolute; bottom: 0px; right: 20px;
+    } 
+.verif {z-index:-2; height: 100vh; width: 100vw; background: black;}</style>
+</head>
+<body>
+	<div class="doc" id="mani">
+	  <div class="cdc">
+<script src="https://www.shoutbox.com/chat/chat.js.php">
+		  </script>
+	</div></div><div class="verif" id="ver"><br><span style="font-size: 200%; background: black; color: lightgreen;"> 	>  <span id="text" style="font-size: 200%; background: black; color: lightgreen;"></span><span class="flsh">|</span></span></div></body>'
+")
