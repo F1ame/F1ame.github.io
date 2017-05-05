d3b6431dbb389cfdde5447a987dab4cd4939d09f
F1ame <keeperflame45@gmail.com>
2017-05-05 13:38:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ cm = cm.substring(1,cm.length);
 	cmdev = eval(cmdev)
 	console.log(cm);
 	console.log(cmdev);
-	prevoutp = document.getElementById("
+	//prevoutp = document.getElementById("outp
 	document.getElementById("outpt").innerHTML = ">" cmdev + "<br>";
 }
 pgnd = "</body></html>";
