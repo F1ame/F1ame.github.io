887cd8fb45389042ed2a280553f2faf373e6b463
F1ame <keeperflame45@gmail.com>
2017-05-10 14:35:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 rstxt = false;
-winext = false
+winext = "N"
 function main(ck) {
  vp = window.open("https://f1ame.github.io","_blank")
-winext = true 
+winext = "Y" 
 }
 Shoutbx = "<iframe id=\"sbx\" WIDTH=\"200\" HEIGHT=\"400\" title=\"Shoutbox\" src=\"https://shoutbox.widget.me/start.html?uid=591uatyg\" frameborder=\"0\" scrolling=\"auto\"></iframe>";
 webAppUrl = "https://script.google.com/macros/s/AKfycbzIkiKjIHtWvw5WeNmNMTIE98i3OUvWYaKjAmrFm9YYr6SKsxUJ/exec"
@@ -15,13 +15,14 @@ var cmdswnd = {
 	
 }
 function doc(tx) {
-	if(winext == true){
+	if(winext == "Y"){
 		vp.document.open("text/html", "replace");
 		vp.document.write(tx);
 		vp.document.close();
 		rtn = "Successfully"
 	}else{
 		rtn="<br>> ERROR<br>> Please Click the Enter Site Button"
+		console.log(winext)
 	}
 	return rtn
 }
